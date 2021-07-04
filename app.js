b55d9ee290b5e91c6cf5f7688b1826a038a5b420
Kade Zimmerman <kaderz0021@gmail.com>
2021-07-03 18:58:37 -0600

    commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+const db = require('./db');
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('views'));
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/create', db.createUser);
+
+app.get('/table', db.displayUsers);
+
+app.post('/table', db.deleteUser);
+
+app.get('/sortOY', db.sortUsers);
+
+app.post('/search', db.search);
+
+app.get('/edit/:userId', db.getUser);
+
+app.post('/edit/:userId', db.editUser);
+
+app.listen(port, () => {
+    console.log(`Listening To Port: ${port}`)
+});
